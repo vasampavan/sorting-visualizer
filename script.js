200d9ce05a1e531d348afd65e5dd2ad988e768d8
Pavan Vasam <162179032+vasampavan@users.noreply.github.com>
2025-01-29 12:40:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let array = [];
 let delay = 500;
 
 function updateDelay() {
-  //delay = (21 - speedInput.value) * 50;
+  delay = (101 - speedInput.value);
   speedValue.textContent = `${delay} ms`;
 }
 
